1d602d8aa8a21e2287605103c0975219035e65bf
Lucas Brum <brum.lucas@icloud.com>
2023-07-18 23:57:03 -0300

    git ignore

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  signupForm = this.formBuilder.group({
+    nome: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  onSubmitLoginForm(): void {
+    console.log('Dados do Formulário de Login: ', this.loginForm.value)
+  }
+
+  onSubmitSignupForm(): void {
+    console.log('Dados do Formulário de Login: ', this.signupForm.value)
+  }
 }
