dd09d45d3d898537f87394b35655d0fd3e872d83
lucasbrum <brum.lucas@icloud.com>
2023-07-29 16:54:50 -0300

    criando meu sidenav

---
+++ src/app/shared/components/toolbar-navigation/nav-data.ts
@@ -0,0 +1,21 @@
+export const navbarData = [
+    {
+        label:'Dashboard',
+        routerLink: '/dashboard',
+        icon: "pi pi-home"
+
+    },
+    {
+      label:'Clientes',
+      routerLink: '/clientes',
+      icon: "pi pi-users"
+
+    },
+    {
+      label:'Produtos',
+      routerLink: '/produtos',
+      icon: "pi pi-ticket"
+
+    }
+
+];
--- src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
+++ src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { navbarData } from './nav-data';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 
 @Component({
   selector: 'app-toolbar-navigation',
   templateUrl: './toolbar-navigation.component.html',
-  styleUrls: []
+  styleUrls: ['./toolbar-navigation.component.scss']
 })
 export class ToolbarNavigationComponent {
 
+  @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
+  collapsed = false;
+  screenWidth = 0;
+  navData = navbarData;
+
+  constructor(private cookieService: CookieService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+  }
+
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+  }
+
+  closeSidenav(): void {
+    this.collapsed = false;
+    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+  }
+
+  handleLogout(): void {
+    console.log("callllllllll");
+    this.cookieService.delete('USER_INFO');
+    void this.router.navigate(['/home']);
+  }
 }
